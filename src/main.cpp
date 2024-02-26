d272211c2f42f992b8ec4fc8e951c89eb5e7bdf3
possiblyHugo <alainhandzar@gmail.com>
2024-02-25 22:27:34 -0800

    + Added User Input + Added Calculations + Added output

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
-
 int main() {
+	int base_number = 0;
 
+	std::cout << "Please input the base number: ";
+	std::cin >> base_number;
 
+	for (int i = 1, counter = 1; i <= (base_number / 2); i++, counter++) {
+		if (base_number / i) {
+			std::cout << "Factor Pair " << counter << ": [" << i << "," << base_number / i << "]\n";
+		}
+	}
 
 
 	return 0;
